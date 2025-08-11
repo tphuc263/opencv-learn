e1ee6e14f533693dd247d77af9124bf83df43772
tphuc263 <tphuc.ptit@gmail.com>
2025-08-11 15:57:32 +0700

    basic

---
+++ colors.py
@@ -0,0 +1,25 @@
+"""
+This file defines color constants in BGR (Blue, Green, Red) format
+for use with the OpenCV library.
+"""
+
+# --- Primary Colors ---
+# NOTE: OpenCV uses BGR order, not RGB.
+RED = (0, 0, 255)
+GREEN = (0, 255, 0)
+BLUE = (255, 0, 0)
+
+# --- Secondary Colors ---
+YELLOW = (0, 255, 255)
+CYAN = (255, 255, 0)
+MAGENTA = (255, 0, 255)
+
+# --- Basic Shades ---
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (128, 128, 128)
+
+# --- Other Common Colors ---
+ORANGE = (0, 165, 255)
+PURPLE = (128, 0, 128)
+PINK = (203, 192, 255)
---
+++ rescale.py
@@ -0,0 +1,29 @@
+import cv2 as cv
+
+img = cv.imread('Photos/cat.jpg')
+cv.imshow('cat', img)
+
+def rescaleFrame(frame, scale=0.75):
+    # Images, videos, live video
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+def changeRes(width, height):
+    # live video
+    capture.set(3, width)
+    capture.set(4, height)
+
+capture = cv.VideoCapture('Videos/cat_video.mp4')
+while True:
+    isTrue, frame = capture.read()
+    resize_frame = rescaleFrame(frame, scale=0.4)
+
+    cv.imshow('cat see the sky', frame)
+    cv.imshow('cat see the sky resired', resize_frame)
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+capture.release()
+cv.destroyAllWindows()
